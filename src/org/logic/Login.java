1b8f38770a35cc8ff6e6e3ab255d2e70e33f2e17
Dhanalakshmi2021 <dhanamsekar1221@gmail.com>
2021-11-08 18:20:14 +0530

    Folder Structure

---
+++ src/org/logic/Login.java
@@ -0,0 +1,19 @@
+package org.logic;
+
+public class Login {
+	
+	public static void main(String[] args) {
+		  
+		int a=11;
+	if (a%2==0) {
+		System.out.println(a+" is even number");
+		
+	} else {
+		System.out.println(a+" is odd number");
+
+	}
+	}
+ 
+	
+}
+	
