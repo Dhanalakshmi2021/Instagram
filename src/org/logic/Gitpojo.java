7fd9e2bde45d0f66a78c01c3d92828f609b402b7
Dhanalakshmi2021 <dhanamsekar1221@gmail.com>
2021-11-09 11:55:24 +0530

    password functionality

--- src/org/logic/Gitpojo.java
+++ src/org/logic/Gitpojo.java
@@ -1,16 +1,13 @@
 package org.logic;
 
 public class Gitpojo {
-	public static void main(String[] args) {
-
-
-		for (int i = 0; i <=100; i++) {
-			if (i%2==0) {
-				System.out.println(i);
-				
-			}
-			
-		}
+	
+	public static void print() {
+		System.out.println("password Function");
 	}
 
+	public static void main(String[] args) {
+		print();
+	}
 }
+	
